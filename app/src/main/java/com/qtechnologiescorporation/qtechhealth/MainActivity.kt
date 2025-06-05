3854932d24404eb6a48d9b62f2b76b699053609c
QTechnologiesCorporation <qtechnologies@ramzasinvest.com>
2025-06-05 09:39:56 +0530

    features - tested

--- app/src/main/java/com/qtechnologiescorporation/qtechhealth/MainActivity.kt
+++ app/src/main/java/com/qtechnologiescorporation/qtechhealth/MainActivity.kt
@@ -41,6 +41,7 @@ class MainActivity : ComponentActivity() {
                     navigationManager.setController(navController)
                     onDispose {
                         navigationManager.clear()
+
                     }
                 }
 
